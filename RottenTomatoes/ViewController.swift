3bca372da913e3ec1e59587847c89ec8c962499a
David Kuchar <davidkuchar@gmail.com>
2015-05-04 20:26:09 -0700

    initial cocoapods install, with a table view, etc

--- RottenTomatoes/ViewController.swift
+++ RottenTomatoes/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    
+    
+    
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
